6cc3f76f043cf7da015ed51404607615e50d4d17
Duda Andrada <duda.andrada@isr.uc.pt>
2026-06-05 21:33:22 +0100

    refactor(bag): wire up public __init__.py exports for RosbagUtils, Ros2BagUtils

--- src/fruc_ros_utils/bag/__init__.py
+++ src/fruc_ros_utils/bag/__init__.py
@@ -1,1 +1,11 @@
 """ROS bag utility modules for ROS1/ROS2 processing and conversion."""
+
+from .ouster_encode import OusterPointCloudEncoder
+from .bagutils import RosbagUtils
+from .ros2utils import Ros2BagUtils
+
+__all__ = [
+    "OusterPointCloudEncoder",
+    "RosbagUtils",
+    "Ros2BagUtils",
+]
